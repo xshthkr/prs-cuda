e4620b79b16fe62128dbf016369b462272634167
xshthkr <shashankt2004@gmail.com>
2025-04-11 13:52:04 +0530

    initial commit

---
+++ src/prs-cuda.cu
@@ -0,0 +1,27 @@
+#include <cuda.h>
+
+#include <stdio.h>
+
+void getCudaStats();
+
+int main() {
+        getCudaStats();
+        return 0;
+}
+
+void getCudaStats() {
+        int deviceCount;
+        cudaGetDeviceCount(&deviceCount);
+        printf("Number of CUDA devices: %d\n", deviceCount);
+        
+        for (int i = 0; i < deviceCount; i++) {
+                cudaDeviceProp prop;
+                cudaGetDeviceProperties(&prop, i);
+                printf("Device %d: %s\n", i, prop.name);
+                printf("  Total global memory: %zu bytes\n", prop.totalGlobalMem);
+                printf("  Multiprocessor count: %d\n", prop.multiProcessorCount);
+                printf("  Max threads per block: %d\n", prop.maxThreadsPerBlock);
+        }
+        
+        return;
+}

00bdc57b59841cb25e1a2e968d3a1f97f1e8473f
xshthkr <shashankt2004@gmail.com>
2025-04-16 15:57:07 +0530

    integer rng, min, max

--- include/utils.h
+++ include/utils.h
@@ -3,14 +3,24 @@
 
 #include <stdint.h>
 
-uint8_t gen_random(
-        uint8_t lowerbound,
-        uint8_t upperbound
+int8_t gen_random(
+        int8_t lowerbound,
+        int8_t upperbound
 );
 
 double eval_fitness(
         const double* solution,
-        const uint32_t* dim
+        const uint32_t dim
+);
+
+double max(
+        const double* x,
+        const uint32_t dim
+);
+
+double min(
+        const double* x,
+        const uint32_t dim
 );
 
 #endif // UTILS_H
--- src/utils.c
+++ src/utils.c
@@ -6,15 +6,15 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-uint8_t gen_random(uint8_t lower, uint8_t upper) {
+int8_t gen_random(int8_t lower, int8_t upper) {
         if (lower > upper) {
-                uint64_t temp = lower;
+                int8_t temp = lower;
                 lower = upper;
                 upper = temp;
         }
 
         uint8_t range = upper - lower + 1;
-        uint8_t rand_val;
+        int8_t rand_val;
         int fd = open("/dev/urandom", O_RDONLY);
         if (fd < 0) {
                 perror("Failed to open /dev/urandom");
@@ -32,13 +32,33 @@ uint8_t gen_random(uint8_t lower, uint8_t upper) {
         } while ((uint8_t)rand_val >= limit);
 
         close(fd);
-        return lower + ((uint8_t)rand_val % range);
+        return lower + ((int8_t)rand_val % range);
 }
 
-double eval_fitness(const double* x, const uint32_t* dim) {
+double eval_fitness(const double* x, const uint32_t dim) {
         double fitness = 0.0;
-        for (uint32_t i = 0; i < *dim; i++) {
+        for (uint32_t i = 0; i < dim; i++) {
                 fitness += x[i] * x[i];
         }
         return fitness;
+}
+
+double max(const double* x, const uint32_t dim) {
+        double max_val = x[0];
+        for (uint32_t i = 1; i < dim; i++) {
+                if (x[i] > max_val) {
+                        max_val = x[i];
+                }
+        }
+        return max_val;
+}
+
+double min(const double* x, const uint32_t dim) {
+        double min_val = x[0];
+        for (uint32_t i = 1; i < dim; i++) {
+                if (x[i] < min_val) {
+                        min_val = x[i];
+                }
+        }
+        return min_val;
 }

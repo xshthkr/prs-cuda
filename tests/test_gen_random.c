9d1bd51b2ce170e1ddaf2dc4ba69d6627c89bd2f
xshthkr <shashankt2004@gmail.com>
2025-04-16 16:42:35 +0530

    test rng unsigned and double

--- tests/test_gen_random.c
+++ tests/test_gen_random.c
@@ -9,7 +9,7 @@ int main () {
         /* test 1 */
         uint8_t lower = 0;
         uint8_t upper = 90;
-        uint8_t random_value = gen_random(lower, upper);
+        uint8_t random_value = gen_random_unsigned(lower, upper);
         assert(random_value >= lower && random_value <= upper);
 
         // int loop = 0;
@@ -20,6 +20,12 @@ int main () {
         //         assert(random_value >= lower && random_value <= upper);
         //         loop++;
         // }
+
+        /* test 2 */
+        double lower_d = -1.0;
+        double upper_d = 1.0;
+        double random_value_double = gen_random_double(lower_d, upper_d);
+        assert(random_value_double >= lower_d && random_value_double <= upper_d);
 
         printf("All tests passed.\n");
         return 0;
